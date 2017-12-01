625a72c39d3a8638fc065d91bdab9d55430cb5dd
jdfloresl <jdfloresl@utn.edu.ec>
2017-11-30 21:36:49 -0500

    init

--- src/main/java/zede/ZedeServidorWebApplication.java
+++ src/main/java/zede/ZedeServidorWebApplication.java
@@ -6,15 +6,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan(basePackages={"zede.model"})
 @EnableJpaRepositories(basePackages = "zede.repository")
 @ComponentScan(basePackages={"zede.controller", "zede.utilities"})
 @EnableTransactionManagement
+
 public class ZedeServidorWebApplication {
+	  @RequestMapping("/")
+	    @ResponseBody
+	    String home() {
+	      return "Hello World!";
+	    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZedeServidorWebApplication.class, args);
 	}
